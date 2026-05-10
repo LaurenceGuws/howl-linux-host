02ab9c5b4b4a9a95b1ecdf5a189445dadf5c465e
Laurence <lggousie46@gmail.com>
2026-05-10 16:43:08 +0200

    linux-host: use term input contract

--- src/terminal/input.zig
+++ src/terminal/input.zig
@@ -3,65 +3,66 @@
 //! Reason: keep host event vocabulary separate from terminal runtime input.
 
 const Input = @import("../input/input.zig").Input;
-const HowlTerm = @import("howl_term").HowlTerm;
+const term = @import("howl_term");
+const TermInput = term.Input;
 
-pub fn key(key_event: Input.Key) ?HowlTerm.Key {
+pub fn key(key_event: Input.Key) ?TermInput.Key {
     return switch (key_event) {
-        .escape => HowlTerm.key_escape,
-        .tab => HowlTerm.key_tab,
-        .enter => HowlTerm.key_enter,
-        .backspace => HowlTerm.key_backspace,
-        .insert => HowlTerm.key_insert,
-        .delete => HowlTerm.key_delete,
-        .home => HowlTerm.key_home,
-        .end => HowlTerm.key_end,
-        .page_up => HowlTerm.key_pageup,
-        .page_down => HowlTerm.key_pagedown,
-        .up => HowlTerm.key_up,
-        .down => HowlTerm.key_down,
-        .left => HowlTerm.key_left,
-        .right => HowlTerm.key_right,
-        .f1 => HowlTerm.key_f1,
-        .f2 => HowlTerm.key_f2,
-        .f3 => HowlTerm.key_f3,
-        .f4 => HowlTerm.key_f4,
-        .f5 => HowlTerm.key_f5,
-        .f6 => HowlTerm.key_f6,
-        .f7 => HowlTerm.key_f7,
-        .f8 => HowlTerm.key_f8,
-        .f9 => HowlTerm.key_f9,
-        .f10 => HowlTerm.key_f10,
-        .f11 => HowlTerm.key_f11,
-        .f12 => HowlTerm.key_f12,
+        .escape => TermInput.key_escape,
+        .tab => TermInput.key_tab,
+        .enter => TermInput.key_enter,
+        .backspace => TermInput.key_backspace,
+        .insert => TermInput.key_insert,
+        .delete => TermInput.key_delete,
+        .home => TermInput.key_home,
+        .end => TermInput.key_end,
+        .page_up => TermInput.key_pageup,
+        .page_down => TermInput.key_pagedown,
+        .up => TermInput.key_up,
+        .down => TermInput.key_down,
+        .left => TermInput.key_left,
+        .right => TermInput.key_right,
+        .f1 => TermInput.key_f1,
+        .f2 => TermInput.key_f2,
+        .f3 => TermInput.key_f3,
+        .f4 => TermInput.key_f4,
+        .f5 => TermInput.key_f5,
+        .f6 => TermInput.key_f6,
+        .f7 => TermInput.key_f7,
+        .f8 => TermInput.key_f8,
+        .f9 => TermInput.key_f9,
+        .f10 => TermInput.key_f10,
+        .f11 => TermInput.key_f11,
+        .f12 => TermInput.key_f12,
         else => null,
     };
 }
 
-pub fn mods(input_mods: Input.Mod) HowlTerm.Modifier {
-    var out: HowlTerm.Modifier = 0;
-    if (input_mods.shift) out |= HowlTerm.mod_shift;
-    if (input_mods.alt) out |= HowlTerm.mod_alt;
-    if (input_mods.ctrl) out |= HowlTerm.mod_ctrl;
+pub fn mods(input_mods: Input.Mod) TermInput.Modifier {
+    var out: TermInput.Modifier = 0;
+    if (input_mods.shift) out |= TermInput.mod_shift;
+    if (input_mods.alt) out |= TermInput.mod_alt;
+    if (input_mods.ctrl) out |= TermInput.mod_ctrl;
     return out;
 }
 
-pub fn mouseKind(kind: Input.Mouse.Kind) HowlTerm.MouseEventKind {
+pub fn mouseKind(kind: Input.Mouse.Kind) TermInput.MouseEventKind {
     return switch (kind) {
-        .move => HowlTerm.mouse_move,
-        .press => HowlTerm.mouse_press,
-        .release => HowlTerm.mouse_release,
-        .wheel => HowlTerm.mouse_wheel,
+        .move => TermInput.mouse_move,
+        .press => TermInput.mouse_press,
+        .release => TermInput.mouse_release,
+        .wheel => TermInput.mouse_wheel,
     };
 }
 
-pub fn mouseButton(button: Input.Mouse.Button) HowlTerm.MouseButton {
+pub fn mouseButton(button: Input.Mouse.Button) TermInput.MouseButton {
     return switch (button) {
-        .none => HowlTerm.mouse_button_none,
-        .left => HowlTerm.mouse_button_left,
-        .middle => HowlTerm.mouse_button_middle,
-        .right => HowlTerm.mouse_button_right,
-        .wheel_up => HowlTerm.mouse_button_wheel_up,
-        .wheel_down => HowlTerm.mouse_button_wheel_down,
+        .none => TermInput.mouse_button_none,
+        .left => TermInput.mouse_button_left,
+        .middle => TermInput.mouse_button_middle,
+        .right => TermInput.mouse_button_right,
+        .wheel_up => TermInput.mouse_button_wheel_up,
+        .wheel_down => TermInput.mouse_button_wheel_down,
     };
 }
 
